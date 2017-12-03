5a2018b4e50352bc29b5fc7aec3932c295a52a73
AHDevelop <hatakehirodev@gmail.com>
2017-12-03 09:51:58 +0900

    TOP画面の家事履歴入力を実装

--- www/js/common.js
+++ www/js/common.js
@@ -16,62 +16,38 @@ function showInviteModal (){
 /*
 * 家事登録ダイアログで家事時間を加算する
 */
-function plusHour(obj){
-  var targetElm = $(obj).siblings(".hourLabel");
-  var hour =  Number(targetElm.html());
-
-  if(24 <= hour){
-    return false;
-  }
-  hour += 0.5;
-
-  var hourStr = hour.toString();
-  if(!hourStr.match(/\./)){
-    hourStr += ".0"
-  }
-
-  targetElm.html(hourStr);
+function plusHour(hour){
+    
+    var hour =  Number(hour);
+    
+    if(hour < 24){
+        hour += 0.5;
+    }
+    
+    var hourStr = hour.toString();
+    if(!hourStr.match(/\./)){
+        hourStr += ".0"
+    }
+    
+    return hourStr;
 }
 
 /*
 * 家事登録ダイアログで家事時間を減算する
 */
-function minusHour(obj){
-  var targetElm = $(obj).siblings(".hourLabel");
-  var hour =  Number(targetElm.html());
-
-  if(hour <= 0){
-    return false;
-  }
-
-  hour -= 0.5;
-  var hourStr = hour.toString();
-
-  if(!hourStr.match(/\./)){
-    hourStr += ".0"
-  }
-
-  targetElm.html(hourStr);
+function minusHour(hour){
+    
+    var hour =  Number(hour);
+    
+    if(0 < hour){
+        hour -= 0.5;
+    }
+    
+    var hourStr = hour.toString();
+    
+    if(!hourStr.match(/\./)){
+        hourStr += ".0"
+    }
+    
+    return hourStr;
 }
-
-// function callApi(url, param){
-//
-//   var deferred = $.Deferred();
-//
-//   $.ajax({
-//       url: url,
-//       type: 'GET',
-//       data: param,
-//       dataType: 'json',
-//       error: function(jqXHR, text_status, strError) {
-//         alert("通信に失敗しました。  " + text_status + "  " + strError);
-//         deferred.reject(jqXHR);
-//       },
-//       success: function(data) {
-//           deferred.resolve(data);
-//       }
-//   });
-//
-//   return deferred.promise();
-//
-// }
--- www/js/initialize.js
+++ www/js/initialize.js
@@ -128,44 +128,52 @@ module.controller("topPageController", function($scope) {
 
    var roomId = roomInfo.room_id;
    
-   getHomeWorkListWithRoomId(roomId).done(function(response){
-     $scope.roomHomeworkList = response;
+    getHomeWorkListWithRoomId(roomId).done(function(response){
+        $scope.roomHomeworkList = response;
 
-     // 最新の情報で更新
-     $scope.$apply();
-   });
+        // 最新の情報で更新
+        $scope.$apply();
+    });
 
    // 家事時間入力ダイアログを表示
    $scope.callInputHomeWorkPage = function(index){
 
-     // IDから該当する家事を特定する
-     var roomHomework = $scope.roomHomeworkList[index];
-
-     // 家事名と標準時間を設定した状態でダイアログを起動する
-     ons.createDialog('inputHomeWork.html', {parentScope: $scope}).then(function(dialog) {
-         $scope.inputHomeWork = dialog;
-         $scope.homeworkName = roomHomework.home_work_name;
-         $scope.roomHomeworkId = roomHomework.room_home_work_id;
-         $scope.beseHomeworkTimeHH = 2; // ToDo APIの返却値に基準時間追加されるの待ち
-         $scope.inputHomeWork.show();
-
-         // 家事登録
-         $scope.inputHomeWork.register = function(roomHomeworkId2){
-            var homeworkTime = Number($("#hourLabel").html());
-            homeworkDate = moment().format('YYYY-MM-DD');;
-            updateHomeworkHist(roomHomeworkId2, homeworkDate, homeworkTime).done(function(response){
-                $scope.inputHomeWork.hide();
-                $scope.$apply();
-                getHomeWorkListWithRoomId(roomId).done(function(response){
-                  $scope.roomHomeworkList = response;
-
-                  // 最新の情報で更新
-                  $scope.$apply();
+        // IDから該当する家事を特定する
+        var roomHomework = $scope.roomHomeworkList[index];
+                
+        // 家事名と標準時間を設定した状態でダイアログを起動する
+        ons.createDialog('inputHomeWork.html', {parentScope: $scope}).then(function(dialog) {
+            $scope.inputHomeWork = dialog;
+            $scope.homeworkName = roomHomework.home_work_name;
+            $scope.roomHomeworkId = roomHomework.room_home_work_id;
+            $scope.beseHomeworkTimeHH = roomHomework.base_home_work_time_hh;
+            $scope.homeworkTimeHH = roomHomework.base_home_work_time_hh;
+            $scope.inputHomeWork.show();
+                        
+            // 家事登録
+            $scope.inputHomeWork.register = function(roomHomeworkId){
+                homeworkDate = moment().format('YYYY-MM-DD');
+                updateHomeworkHist(roomHomeworkId, homeworkDate, $scope.homeworkTimeHH).done(function(response){
+                    $scope.inputHomeWork.hide();
+                    $scope.$apply();
+                    getHomeWorkListWithRoomId(roomId).done(function(response){
+                        $scope.roomHomeworkList = response;
+                        
+                        // 最新の情報で更新
+                        $scope.$apply();
+                    });
                 });
-            });
-         };
-       });
-   }
+            };
+            
+            $scope.inputHomeWork.plusHour = function(){
+                $scope.homeworkTimeHH = plusHour($scope.homeworkTimeHH);
+            };
+            $scope.inputHomeWork.minusHour = function(){
+                $scope.homeworkTimeHH = minusHour($scope.homeworkTimeHH);
+            };
+
+        });
+   };
 });
 
     /*
@@ -187,8 +195,7 @@ module.controller("topPageController", function($scope) {
     module.controller("addHomeworkPageController", function($scope) {
     
       // 部屋家事の取得
-      // TODO roomIdを共通処理から取得する
-      var roomId ="2";
+      var roomId =roomInfo.room_id;
       getRoomHomework(roomId).done(function(response){
         $scope.roomHomeworkList = response;
         console.log($scope.roomHomeworkList[0]);
@@ -243,27 +250,27 @@ module.controller("topPageController", function($scope) {
     $("#isDispTopListLabel").html($("#isVisible").prop('checked') ? "する" : "しない");
   }
 
-  // 部屋家事の新規登録処理
-  $scope.insertRoomHomework = function(isDispTopList){
-
-    var record = [];
-    var homework = {};
-
-    homework['home_work_name'] = $("#workname").val();
-    homework['base_home_work_time'] = Number($("#hourLabel").html());
-    homework['room_home_work_id'] = $("#roomHomeworkId").val();
-    homework['is_visible'] = $("#isVisible").prop('checked');
-    record.push(homework);
-
-    updateRoomHomework('1', '2', record).done(function(response){
-      getRoomHomework(roomId).done(function(response){
-        $scope.roomHomeworkList = response;
-        $scope.$apply();
-      });
-    });
-
-    $scope.editPageDialog.hide();
-  }
+    // 部屋家事の新規登録処理
+    $scope.insertRoomHomework = function(isDispTopList){
+    
+        var record = [];
+        var homework = {};
+        
+        homework['home_work_name'] = $("#workname").val();
+        homework['base_home_work_time'] = Number($("#hourLabel").html());
+        homework['room_home_work_id'] = $("#roomHomeworkId").val();
+        homework['is_visible'] = $("#isVisible").prop('checked');
+        record.push(homework);
+        
+        updateRoomHomework(userInfo.user_id, roomInfo.room_id, record).done(function(response){
+            getRoomHomework(roomId).done(function(response){
+                $scope.roomHomeworkList = response;
+                $scope.$apply();
+            });
+        });
+        
+        $scope.editPageDialog.hide();
+    }
 
     // 部屋家事の削除
     $scope.deleteRoomHomework = function(){
@@ -274,7 +281,7 @@ module.controller("topPageController", function($scope) {
         homework['room_home_work_id'] = $("#roomHomeworkId").val();
         record.push(homework);
         
-        deleteRoomHomework('1', '2', record).done(function(response){
+        deleteRoomHomework(userInfo.user_id, roomInfo.room_id, record).done(function(response){
             getRoomHomework(roomId).done(function(response){
                 $scope.roomHomeworkList = response;
                 $scope.$apply();
@@ -283,16 +290,13 @@ module.controller("topPageController", function($scope) {
         
         $scope.editPageDialog.hide();
         }
-    
     });
 
     /*
     招待ページコントローラ
     */
     module.controller("invitePageController", function($scope) {
-    
-      //$scope.isDispTopList = null;
-    
+        
       // Show Dialog
       $scope.callInvitePage = function(obj){
     
@@ -302,77 +306,3 @@ module.controller("topPageController", function($scope) {
           });
       }
     });
-
-/*
-TEST APIページコントローラ
-*/
-// module.controller("testAPIPageController", function($scope) {
-//   var user = {
-//       add: function(){
-//           window.console.log('test');
-// 
-//           window.console.log(monaca);
-//           monaca.cloud.User.register("me@example.com", "password", {age:21})
-//             .done(function(result)
-//             {
-//                window.console.log("Welcome, " + result.user._username);
-//                window.console.log("You are " + result.user.age + " years old.");
-//             }
-//             )
-//             .fail(function(err)
-//             {
-//                window.console.log("Err#" + err.code +": " + err.message);
-//             })
-//             .always(function(){
-//               window.console.log('aaa');
-//             });
-//             window.console.log('test2');
-//       },
-//   };
-// 
-//   $scope.user = user;
-
-  // $scope.test = function()
-  // {
-  //     monaca.cloud.User.register("me@example.com", "password", {age:21})
-  //           .done(function(result)
-  //           {
-  //              window.console.log("Welcome, " + result.user._username);
-  //              window.console.log("You are " + result.user.age + " years old.");
-  //           }
-  //           )
-  //           .fail(function(err)
-  //           {
-  //              window.console.log("Err#" + err.code +": " + err.message);
-  //           })
-  //           .always(function(){
-  //             window.console.log('aaa');
-  //           });
-  //           window.console.log('test2');
-  // };
-//   if (false == monaca.cloud.User.isAuthenticated()) {
-//       window.console.log('test33');
-//   } else {
-//       window.console.log('test4');
-//   }
-// 
-//   window.console.log('login:' + monaca.cloud.User.isAuthenticated());
-// 
-//   monaca.cloud.User.login("me@example.com", "password")
-//   .done(function(result){
-//      window.console.log("Hello again, " + result.user._username);
-//   })
-//   .fail(function(err)
-//   {
-//      window.console.log("Err#" + err.code +": " + err.message);
-//   })
-//   .always(function(){
-//       window.console.log('always login')
-//   });
-// 
-// var User = monaca.cloud.Collection("User"); // returns Collection object
-// window.console.log("id:"+User.id);
-// window.console.log("name:"+User.name);
-// window.console.log(User);
-
-// });
